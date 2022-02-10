48f23ccda7335abfd037441a5b2bfddc6ee963f2
simziky <simeonehioze@gmail.com>
2022-02-11 00:44:08 +0100

    first

---
+++ main.js
@@ -0,0 +1,28 @@
+var num = 5;
+
+while (num < 50) {
+    console.log(num)
+    num +=5
+}
+
+
+var grade = 20;
+
+    if (grade <= 100 && grade >= 80) {
+                console.log("grade O")
+    }
+    else if (grade <= 75 && grade >= 65) {
+        console.log("grade A")
+    }
+    else if (grade <= 55 && grade >= 45) {
+        console.log("grade B")
+    }
+    else if (grade <= 35 && grade >= 25) {
+        console.log("grade C")
+    }
+    else if (grade < 15 && grade >= 5) {
+        console.log("grade D")
+    }
+    else {
+        console.log("Failed")
+    }
